23750a87e717a94c076cf879c2cff5d22829feee
SaiPrasanna1996 <72405922+SaiPrasanna1996@users.noreply.github.com>
2022-03-12 15:46:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <section className="main">
+  <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Prasu images" />
+  <h2>SETTY VENKATA SIVA NAGA SAI  PRASANNA</h2>
+  <h3>178A1F0016</h3>
+  <h3>Master Of Computer Applications</h3>  
+<h1>Languages Known:</h1>
+<ul type="square">
+  <li>Telugu</li>
+  <li>English</li>
+</ul>
+<h2>Hobbies:</h2>
+<ul type="square">
+  <li>Cooking</li>
+  <li>Listening Music</li>
+  <li>Gardening</li>
+  <li>Playing Carroms</li>
+</ul>
+</section>
+<section className="main">
+  <article className="content">
+    <section>
+      <h2>Carrer Objective:</h2>
+      <p>To work in a simulating environment where I can apply and enchance my knowledge and skill to serve the firm to the best of my efforts.</p>
+    </section>
+    <hr/>
+    <h2>Education:</h2>
+  
+    <table border="1">
+      <tr>
+        <th>S.No</th>
+        <th>Qualifications</th>
+        <th>Name Of the Institutions</th>
+        <th>Year of Passing</th>
+        
+      </tr>
+      <tr>
+        <td>1.</td>
+        <td>Master Of Computer Applications</td>
+        <td>RISE Krishna Sai Prakasam Group Of Institution,Ongole</td>
+        <td><center>2020</center></td>      
+      </tr>  
+      <tr>
+        <td>2.</td>
+        <td>Bachelors Degree</td>
+        <td>Sri Rama Krishna Degree College,Ongole</td>
+        <td><center>2017</center></td>
+        
+      </tr>
+      <tr>
+        <td>3.</td>
+        <td>Intermediate(MPC)</td>
+        <td>Sri Venus Junior College,Ongole</td>
+        <td><center>2013</center></td>
+        
+      </tr>
+      <tr>
+        <td>4.</td>
+        <td>S.S.C</td>
+        <td>D.R.R.M.M.High School,Ongole</td>
+        <td><center>2011</center></td>
+        
+        </tr>
+    </table>
+<section>
+  <h2>Technical skills:</h2>
+  <ul type="square">
+    <li>C programming</li>
+    <li>Java programming</li>
+    <li>html</li>
+    <li>css</li>
+    <li>python</li>
+    <li>JavaScript</li>
+  </ul>
+</section>
+<section>
+  <h2>Achievements:</h2>
+  <ul type="square">
+    <li>Participated in workshop on python for three days conducted by NIT Nagaland</li>
+    <li>Participated Seminars</li>
+    </ul>
+  
+</section>
+<section>
+  <h2>Personality Traits:</h2>
+  <ul type="square">
+    <li>Flexibilty and Adaptability</li>
+    <li>Self Learning</li>
+    <li>Honesty</li>
+  </ul>
+</section>
+
+</article>
+<footer>
+    <a href="https://www.linkedin.com/in/setty-venkata-siva-naga-sai-prasanna-043bab187">linkedin</a>
+</footer>
+</section>
+    </div>
+  );
+}
+
+export default App;
