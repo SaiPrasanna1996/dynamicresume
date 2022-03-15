f43d4c0760d008b8bf6b022c14be6740928a5bc6
SaiPrasanna1996 <72405922+SaiPrasanna1996@users.noreply.github.com>
2022-03-15 18:34:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import logo from './logo.svg';
+
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [resumedata, setResumeData] = React.useState([]);
+  React.useEffect(()=>{setResumeData([
+    {
+    
+      "name": "Setty Venkata Siva Naga Sai Prasanna",
+      "edu": "MCA graduate",
+      "hno": "178A1F0016",
+    "skills": ["Java ", "Python", "Sql", "Html", "Css", "JavaScript"],
+    "languages": ["English", "Telugu","Hindi"],
+    "hobbies": ["Cooking","Listening Music","Gardening","Playing Carroms"],
+    "achievements": ["Participated in workshop on phython for 3days conductted by NIT Nagaland","Participated Seminars"],
+   
+  "education": [
+  {
+    "qul": "Master of computer Applications",
+    "clgname": "RISE Krishna Sai Prakasam Group of Institutions, Ongole",
+    "yop": "2020"
+  
+  },
+  {
+    "qul": "B.Sc",
+    "clgname": "S.R.K Degree college, Ongole",
+    "yop": "2017"
+  },
+  {
+    "qul":"Intermediate",
+    "clgname": "Venus Junior college, Ongole",
+    "yop": "2013"
+  },
+  {
+    "qul": "S.S.C",
+    "clgname": "D.R.R.M.M.High School, Ongole",
+    "yop": "2011"
+  
+  },
+  ],
+ 
+   
+}
+  ]
+  )},[])
   return (
     <div className="App">
-     
+      {resumedata.map((item)=>{return(<div>
      <section className="main">
   <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Prasu images" />
-  <h2>SETTY VENKATA SIVA NAGA SAI  PRASANNA</h2>
-  <h3>178A1F0016</h3>
-  <h3>Master Of Computer Applications</h3>  
-<h1>Languages Known:</h1>
+
+
+   <h3>{item.hno}</h3>
+   <h3>{item.edu}</h3>
+   <h1>Languages Known:</h1>
+   <ul type="square">
+     {item.languages.map((lang)=>{return(<li>{lang}</li>)})}
+
+  </ul>
+  <h2>Hobbies:</h2>
 <ul type="square">
-  <li>Telugu</li>
-  <li>English</li>
-</ul>
-<h2>Hobbies:</h2>
-<ul type="square">
-  <li>Cooking</li>
-  <li>Listening Music</li>
-  <li>Gardening</li>
-  <li>Playing Carroms</li>
+
+{item.hobbies.map((hob)=>{return(<li>{hob}</li>)})}
+  
 </ul>
 </section>
+   
 <section className="main">
   <article className="content">
     <section>
@@ -33,74 +76,48 @@ function App() {
     <h2>Education:</h2>
   
     <table border="1">
-      <tr>
-        <th>S.No</th>
+    <tr>
+        
         <th>Qualifications</th>
         <th>Name Of the Institutions</th>
         <th>Year of Passing</th>
         
       </tr>
-      <tr>
-        <td>1.</td>
-        <td>Master Of Computer Applications</td>
-        <td>RISE Krishna Sai Prakasam Group Of Institution,Ongole</td>
-        <td><center>2020</center></td>      
-      </tr>  
-      <tr>
-        <td>2.</td>
-        <td>Bachelors Degree</td>
-        <td>Sri Rama Krishna Degree College,Ongole</td>
-        <td><center>2017</center></td>
-        
-      </tr>
-      <tr>
-        <td>3.</td>
-        <td>Intermediate(MPC)</td>
-        <td>Sri Venus Junior College,Ongole</td>
-        <td><center>2013</center></td>
-        
-      </tr>
-      <tr>
-        <td>4.</td>
-        <td>S.S.C</td>
-        <td>D.R.R.M.M.High School,Ongole</td>
-        <td><center>2011</center></td>
-        
-        </tr>
+     
+      {item.education.map((edu)=>{
+        return(
+          <tr>
+          <td>{edu.qul}</td>
+          <td>{edu.clgname}</td>
+          <td><center>{edu.yop}</center></td>      
+        </tr>  
+      )})}
+      
+     
+      
     </table>
 <section>
   <h2>Technical skills:</h2>
   <ul type="square">
-    <li>C programming</li>
-    <li>Java programming</li>
-    <li>html</li>
-    <li>css</li>
-    <li>python</li>
-    <li>JavaScript</li>
+  {item.skills.map((skill)=>{return(<li>{skill}</li>)})}
   </ul>
 </section>
 <section>
   <h2>Achievements:</h2>
   <ul type="square">
-    <li>Participated in workshop on python for three days conducted by NIT Nagaland</li>
-    <li>Participated Seminars</li>
+  {item.achievements.map((arc)=>{return(<li>{arc}</li>)})}
     </ul>
   
 </section>
-<section>
-  <h2>Personality Traits:</h2>
-  <ul type="square">
-    <li>Flexibilty and Adaptability</li>
-    <li>Self Learning</li>
-    <li>Honesty</li>
-  </ul>
-</section>
 
 </article>
+
 <footer>
     <a href="https://www.linkedin.com/in/setty-venkata-siva-naga-sai-prasanna-043bab187">linkedin</a>
 </footer>
 </section>
+</div>)
+ })}
     </div>
   );
 }
